cbf98ffc1ca6a09f38686d779bde95aced98e8ef
Sarvar55 <sarvar.musazade@gmail.com>
2024-08-14 20:38:50 +0300

    feat: add new domain logic for order service
    This commit introduces new domain logic for the order service, including:
    
    - Adding a new entity, `Customer`, to represent customers placing orders.
    - Adding a new entity, `Restaurant`, to

--- order-service/order-domain/order-domain-core/src/main/java/com/food/order/system/order/service/domain/valueobject/OrderItemId.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/order/system/order/service/domain/valueobject/OrderItemId.java
@@ -1,2 +1,9 @@
-package com.food.order.system.order.service.domain.valueobject;public class OrderItemId {
+package com.food.order.system.order.service.domain.valueobject;
+
+import com.food.order.system.domain.valueobject.BaseId;
+
+public class OrderItemId extends BaseId<Long> {
+    public OrderItemId(Long value) {
+        super(value);
+    }
 }
--- order-service/order-domain/order-domain-core/src/main/java/com/food/order/system/order/service/domain/valueobject/TrackingId.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/order/system/order/service/domain/valueobject/TrackingId.java
@@ -1,2 +1,11 @@
-package com.food.order.system.order.service.domain.valueobject;public class TrackingId {
+package com.food.order.system.order.service.domain.valueobject;
+
+import com.food.order.system.domain.valueobject.BaseId;
+
+import java.util.UUID;
+
+public class TrackingId extends BaseId<UUID> {
+    public TrackingId(UUID value) {
+        super(value);
+    }
 }
